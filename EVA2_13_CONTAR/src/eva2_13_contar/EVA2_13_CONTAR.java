64c3e732e595a35e90d9ff6ca18d594413e8f071
Daniel Navarrete Carmona <danniiel.dn@gmail.com>
2024-10-27 19:43:15 -0600

    Add files via upload

---
+++ EVA2_13_CONTAR/src/eva2_13_contar/EVA2_13_CONTAR.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_13_contar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_13_CONTAR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner (System.in);
+        
+        int masvocales = 0;
+        int i = 0;
+        String letras;
+        
+        System.out.print("Ingresa una cadena de texto: ");
+        letras = captu.nextLine();
+        
+        while (i < letras.length())
+        
+        
+        
+    }
+    
+}
